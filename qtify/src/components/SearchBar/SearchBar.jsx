03c4d11e8c658cc0d8bc30d23e9c21f7adf1abbe
ajeyanadig <ajeyanadig@gmail.com>
2025-05-21 16:39:22 +0530

    Milestone 1 take 3

--- qtify/src/components/SearchBar/SearchBar.jsx
+++ qtify/src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Search.module.css";
+import styles from "./SearchBar.module.css";
 import { ReactComponent as SearchIcon } from "../../assets/search-icon.svg";
 import { useAutocomplete } from "@mui/base/useAutocomplete";
 import { styled } from "@mui/system";
